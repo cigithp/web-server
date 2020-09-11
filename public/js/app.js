2df81f062a49d48259e3074e80b5fe83fe5101ec
Cigith <cigithpillai@gmail.com>
2020-09-10 20:10:35 -0700

    event listener + form submit

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,23 @@
 console.log('Client side javascript')
 
-fetch('http://localhost:5000/weather?address=Boston').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
+const weatherForm = document.querySelector('form')
+const searchText = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const location = searchText.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetch('http://localhost:5000/weather?address='+location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+            
+        })
     })
 })
